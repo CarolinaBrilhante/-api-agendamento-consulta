8525bd5f63195f61c01a749676b676e79bc5decc
CarolinaBrilhante <carolbrilhante@msn.com>
2023-01-04 21:36:50 -0300

    Implementando biblioteca de token com JWT e atualizando as classes.

--- src/main/java/med/arjos/api/controller/AutenticacaoController.java
+++ src/main/java/med/arjos/api/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.arjos.api.controller;
 
 import jakarta.validation.Valid;
 import med.arjos.api.domain.usuario.DadosAutenticacao;
+import med.arjos.api.domain.usuario.Usuario;
+import med.arjos.api.infra.security.DadosTokenJWT;
+import med.arjos.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,11 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-       var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var autentication = manager.authenticate(token);
+            var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+            var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+            var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+            return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+
     }
 }
---
+++ src/main/java/med/arjos/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package med.arjos.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
