224aef433bcd2da958ae38059ff2776a232919f6
CarolinaBrilhante <carolbrilhante@msn.com>
2023-03-22 21:00:37 -0300

    Criação de nova tabela e atualização de classes.

--- src/main/resources/db/migration/V7__create-table-consultas.sql
+++ src/main/resources/db/migration/V7__create-table-consultas.sql
@@ -2,7 +2,7 @@ CREATE TABLE consultas (
 id bigInt not null auto_increment,
 data dateTime not null,
 medico_id bigInt not null,
-paciente_id bigInt not nullL,
+paciente_id bigInt not null,
 primary key(id),
 constraint fk_consultas_medico_id foreign key(medico_id) references medicos(id),
 constraint fk_consultas_paciente_id foreign key(paciente_id) references pacientes(id)
