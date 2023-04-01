691213b65ad4645fb04de047dc839e314b40d2e4
CarolinaBrilhante <carolbrilhante@msn.com>
2023-03-31 21:10:31 -0300

    Implementação da documentação com o SpringDoc.

--- src/main/java/med/arjos/api/controller/ConsultaController.java
+++ src/main/java/med/arjos/api/controller/ConsultaController.java
@@ -1,18 +1,18 @@
 package med.arjos.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.arjos.api.domain.consulta.AgendaDeConsultas;
 import med.arjos.api.domain.consulta.DadosAgendamentoConsulta;
+import med.arjos.api.domain.consulta.DadosCancelamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
     @Autowired
     private AgendaDeConsultas agenda;
