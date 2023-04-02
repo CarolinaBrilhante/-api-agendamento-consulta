30dc1b039c7f3391342d8f21933a661e815e052e
CarolinaBrilhante <carolbrilhante@msn.com>
2023-04-02 15:21:20 -0300

    Iniciando a implementação dos testes com Junit.

--- src/test/java/med/arjos/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/arjos/api/domain/medico/MedicoRepositoryTest.java
@@ -1,16 +1,35 @@
 package med.arjos.api.domain.medico;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class MedicoRepositoryTest {
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     @Test
-    void escolherMedicoAleatorioLivreNaData() {
+    @DisplayName("Deveria devolver null quando unico medico cadastrado nao esta disponivel na data")
+    void escolherMedicoAleatorioLivreNaDataCenario1() {
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA, proximaSegundaAs10);
+        assertThat(medicoLivre).isNull();
+
 
     }
 }
