2919176a3f3056c4273c0c10a727ec9e262ce43f
CarolinaBrilhante <carolbrilhante@msn.com>
2023-04-05 14:13:52 -0300

    Implementando testes.

--- src/test/java/med/arjos/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/arjos/api/controller/ConsultaControllerTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,15 +20,10 @@ class ConsultaControllerTest {
     @DisplayName("Deveria devolver código http 400 quando informações estão inválidas")
     @WithMockUser
     void agendarCenario1() throws Exception {
-        var response = mvc.perform((RequestBuilder) MockServerHttpRequest.post("/consultas"))
+        var response = mvc.perform(post("/consultas"))
         .andReturn().getResponse();
 
         assertEquals(response.getStatus(), HttpStatus.BAD_REQUEST.value());
-
-
-
-
-
 
     }
 
